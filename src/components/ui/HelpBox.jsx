6b021b9816aaccf0e49830cb9075b1ed8a9eb4c4
RaufunNazin <raufun.nazin13@gmail.com>
2025-11-25 12:23:01 +0600

    update helpbox

--- src/components/ui/HelpBox.jsx
+++ src/components/ui/HelpBox.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { UI_ICONS } from "../../utils/icons";
 
 const HelpBox = ({ isEmpty }) => {
@@ -9,51 +9,48 @@ const HelpBox = ({ isEmpty }) => {
         title: "Getting Started",
         points: [
           <>
-            Your network is empty. Click the <b>plus icon</b> below to add your
-            first device.
+            Your network is empty. The <b>plus icon</b> below adds your first
+            device.
           </>,
-          "Once you add a device, other options like Edit Mode will become available.",
+          "Options like Edit Mode become available after adding a device.",
         ],
       }
     : {
         title: "How to Use",
         points: [
           <>
-            Use the <b>Search bar</b> (top right) to find any device.
+            The <b>Search bar</b> (top right) locates any device.
           </>,
 
           <>
-            Click{" "}
             {
               <button className="z-10 rounded-full bg-blue-500 p-1 text-white transition-all duration-200 hover:bg-blue-600">
                 {UI_ICONS.root}
               </button>
             }{" "}
-            to select a new <b>root device</b> for the main view.
+            Selects a new <b>root device</b> for the main view.
           </>,
           <>
-            Click{" "}
             {
               <button className="z-10 p-1 rounded-full text-white transition-all duration-200 hover:bg-green-600 bg-green-500">
                 {UI_ICONS.add}
               </button>
             }{" "}
-            to <b>add</b> a new device to the diagram.
+            <b>Adds</b> a new device to the diagram.
           </>,
           <>
-            Click{" "}
             {
               <button
                 className={`z-10 p-1.5 rounded-full text-white transition-all duration-200 bg-orange-400 hover:bg-orange-400`}
               >
-                {UI_ICONS.edit}
+                {React.cloneElement(UI_ICONS.edit, {
+                  className: `w-2 h-2`,
+                })}
               </button>
             }{" "}
-            to enter <b>Edit Mode</b> to move nodes, create connections, or
-            right-click for more options.
+            Enters <b>Edit Mode</b> to move nodes, create connections, etc.
           </>,
           <>
-            Click{" "}
             {
               <button
                 className={`p-1 bg-[#ef4444] rounded-full text-white transition-all duration-300 ease-in-out hover:bg-[#d43c3c] focus:outline-none cursor-pointer`}
@@ -61,19 +58,17 @@ const HelpBox = ({ isEmpty }) => {
                 {UI_ICONS.undo}
               </button>
             }{" "}
-            to <b>undo</b> the last change in Edit Mode.
+            <b>Undoes</b> the last change in Edit Mode.
           </>,
           <>
-            Click{" "}
             {
               <button className="z-10 rounded-full bg-blue-500 p-1 text-white transition-all duration-200 hover:bg-blue-600">
                 {UI_ICONS.expand}
               </button>
             }{" "}
-            to <b>adjust</b> the view and fit all nodes on screen.
+            <b>Adjusts</b> the view and centers it.
           </>,
           <>
-            Use{" "}
             {
               <button
                 onClick={() => setIsOpen(!isOpen)}
@@ -82,10 +77,9 @@ const HelpBox = ({ isEmpty }) => {
                 {UI_ICONS.reset}
               </button>
             }{" "}
-            to <b>reset</b> the layout.
+            <b>Resets</b> the layout.
           </>,
           <>
-            Use{" "}
             {
               <button
                 className={`relative z-20 p-1 rounded-full text-white bg-blue-500 hover:bg-blue-600 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed`}
@@ -94,10 +88,9 @@ const HelpBox = ({ isEmpty }) => {
                 {UI_ICONS.download}
               </button>
             }{" "}
-            to <b>export</b> the visible diagram.
+            <b>Exports</b> the visible diagram.
           </>,
           <>
-            Use{" "}
             {
               <button
                 className={`p-1 rounded-full text-white transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500 hover:bg-blue-600`}
@@ -106,55 +99,54 @@ const HelpBox = ({ isEmpty }) => {
                 {UI_ICONS.tag}
               </button>
             }{" "}
-            to <b>show/hide</b> cable description.
+            <b>Shows/Hides</b> cable descriptions.
           </>,
           <>
-            Click{" "}
             {
               <button
-                className="px-0 py-1 bg-blue-500 rounded-r-sm shadow-md hover:bg-blue-600 transition-all duration-200 text-white"
+                className="px-0.5 py-1 bg-blue-500 rounded-r-sm shadow-md hover:bg-blue-600 transition-all duration-200 text-white"
                 title="Open Inventory"
               >
                 {UI_ICONS.chevronRight_main}
               </button>
             }{" "}
-            to access your <b>inventory</b>.
+            Accesses your <b>inventory</b>.
           </>,
           <>
-            Connection possible from{" "}
+            Connections are made from{" "}
             <span className="inline-block w-3 h-3 bg-orange-500 rounded-full mr-1" />{" "}
             ⟶{" "}
             <span className="inline-block w-3 h-3 bg-blue-500 rounded-full mr-1" />
           </>,
           <>
-            Click any device to <b>Expand</b> or <b>Collapse</b> its children.
+            Clicking any device <b>Expands</b> or <b>Collapses</b> its children.
           </>,
           <>
-            Hover on{" "}
+            Hovering on{" "}
             {
               <button className="rounded-full p-1 text-slate-400 hover:bg-slate-200 hover:text-slate-700">
                 {UI_ICONS.info}
               </button>
             }{" "}
-            on an ONU node to view <b>customer details.</b>
+            on an ONU node displays <b>customer details.</b>
           </>,
           <>
-            Click on{" "}
+            Clicking on{" "}
             {
               <button className="rounded-full p-1 text-slate-400 hover:bg-slate-200 hover:text-slate-700">
                 {UI_ICONS.info}
               </button>
             }{" "}
-            on any device to view <b>device details.</b>
+            displays <b>device details.</b>
           </>,
           <>
-            Click{" "}
+            Clicking{" "}
             {
               <button className="rounded-full p-1 text-slate-400 hover:bg-slate-200 hover:text-slate-700">
                 {UI_ICONS.chevronRight}
               </button>
             }{" "}
-            on an OLT node to enter <b>OLT view.</b>
+            on an OLT node opens the <b>OLT view.</b>
           </>,
         ],
       };
